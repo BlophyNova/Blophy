b9dc95439604a59bdbd61fb863de7b412a734570
HuaWaterED <1758158706@qq.com>
2024-10-30 17:21:56 +0800

    新增：定时向制谱器发送Ping数据，未收到Pong则自动断开连接

--- Assets/Scripts/Scenes/Title/OK.cs
+++ Assets/Scripts/Scenes/Title/OK.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Data.ChartData;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using WebSocketSharp;
 
@@ -14,18 +15,52 @@ public class OK : MonoBehaviour
     public TMP_InputField ipAddress;
     public Button ok;
     public TMP_Text connectionInfo;
-
+    public WebSocket ws;
     private void Start()
     {
-        ok.onClick.AddListener(() =>
-        {
-            using (var ws = new WebSocket($"ws://{ipAddress.text}:1286/"))
-            {
-                ws.Connect();
-                ws.SendAsync("摩西摩西，我是打谱器，制谱器在吗？",b =>Debug.Log(b));
-            }
-        });
+        ok.onClick.AddListener(() =>StartCoroutine(ConnectEdit()));
+    }
 
+    IEnumerator ConnectEdit()
+    {
+        connectionInfo.text = "正在寻找制谱器中";
+        yield return new WaitForEndOfFrame();
+        ws = new($"ws://{ipAddress.text}:1286/");
         
+        ws.OnOpen += (sender, args) =>StartCoroutine(Jump2Gameplay());
+        //ws.OnError += (sender, args) =>StartCoroutine(ConnectFailed());
+        ws.Connect();
+    }
+
+    IEnumerator ConnectFailed()
+    {
+        connectionInfo.text = "阿巴阿巴，连不上制谱器呀";
+        yield return new WaitForEndOfFrame();
+    }
+    IEnumerator Jump2Gameplay()
+    {
+        connectionInfo.text = "连接成功！即将跳转！";
+        StartCoroutine(PingPong());
+        yield return new WaitForSeconds(3);
+        //SceneManager.LoadScene("GamePlay", LoadSceneMode.Single);
+    }
+
+    IEnumerator PingPong()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(5);
+            if (!ws.Ping())
+            {
+                ws.Close();
+            }
+            
+            
+        }
+    }
+
+    private void OnApplicationQuit()
+    {
+        ws.Close();
     }
 }
